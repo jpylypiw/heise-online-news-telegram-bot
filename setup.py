ee04e770f89d2f390b3e9feac8a4bc6814ded7df
jpylypiw <12394156+jpylypiw@users.noreply.github.com>
2020-01-18 22:21:03 +0100

    setup restructure

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
-import os
+"""this is the setup module for heise-online-news-telegram-bot"""
 from setuptools import setup
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+def readme():
+    """include the readme as long_description"""
+    with open('README.md') as handler:
+        return handler.read()
 
 
 setup(
     name="heise-online-news-telegram-bot",
     version="0.0.1",
+    description="This is the inofficial heise online telegram bot.",
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary'
+    ],
+    keywords="heise telegram telegram-bot python3",
+    url="https://github.com/jpylypiw/heise-online-news-telegram-bot",
     author="Jochen Pylypiw",
     author_email="jochen@pylypiw.com",
-    description=("This is the inofficial heise online telegram bot."),
     license="GPL-3.0",
-    keywords="heise, telegram, telegram-bot, python3",
-    url="https://github.com/jpylypiw/heise-online-news-telegram-bot",
     packages=['heise_online_bot'],
-    long_description=read('README.md'),
-    classifiers=[
-        'Development Status :: 1 - Planning',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Programming Language :: Python',
+    install_requires=[
+        'requests',
+        'python-telegram-bot',
+        'xmltodict'
     ],
+    include_package_data=True,
+    zip_safe=False,
     setup_requires=[
         'pytest-runner',
     ],
